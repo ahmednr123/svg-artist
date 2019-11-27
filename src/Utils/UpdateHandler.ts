bb70c6eaa65ab09c909ceb0c2d687563785de83a
Ahmed Noor <ahmednr123@gmail.com>
2019-11-27 16:35:30 +0530

    UpdateHandler update function defined

--- src/Utils/UpdateHandler.ts
+++ src/Utils/UpdateHandler.ts
@@ -2,6 +2,19 @@ import {SVGElementType} from '../Meta/SVGElementType';
 
 export default class UpdateHandler {
     static update (svgType: SVGElementType, id: string, data: any) {
-        
+        let element = document.getElementById(`#${id}`);
+        if (element == undefined) {
+            console.error("Element doesn't exist!");
+            return;
+        }
+
+        if (element.tagName.toLowerCase() != svgType) {
+            console.error(`Wrong SVGElement!\nExpected = ${svgType}, Found = ${element.tagName.toLowerCase()}`);
+            return;
+        }
+
+        for (let attrib in data) {
+            element.setAttribute(attrib, data[attrib]);
+        }
     }
 }
